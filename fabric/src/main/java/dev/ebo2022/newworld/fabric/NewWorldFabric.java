52b77d44d6bff2a40bc5f9e9752ed4725e3ac45b
ebo2022 <99616031+ebo2022@users.noreply.github.com>
2022-08-19 14:39:25 -0400

    Biome modifications

--- fabric/src/main/java/dev/ebo2022/newworld/fabric/NewWorldFabric.java
+++ fabric/src/main/java/dev/ebo2022/newworld/fabric/NewWorldFabric.java
@@ -1,7 +1,14 @@
 package dev.ebo2022.newworld.fabric;
 
 import dev.ebo2022.newworld.core.NewWorld;
+import dev.ebo2022.newworld.core.registry.NWFeatures;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
+import net.minecraft.tags.BiomeTags;
+import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.biome.Biomes;
+import net.minecraft.world.level.levelgen.GenerationStep;
 
 /**
  * @author ebo2022
@@ -11,5 +18,7 @@ public class NewWorldFabric implements ModInitializer {
     @Override
     public void onInitialize() {
         NewWorld.PLATFORM.setup();
+        BiomeModifications.addFeature(BiomeSelectors.categories(Biome.BiomeCategory.TAIGA), GenerationStep.Decoration.VEGETAL_DECORATION, NWFeatures.Configured.TREES_FIR_SCARCE_KEY);
+        BiomeModifications.addFeature(BiomeSelectors.includeByKey(Biomes.MEADOW), GenerationStep.Decoration.VEGETAL_DECORATION, NWFeatures.Configured.TREES_FIR_MEADOW_KEY);
     }
 }
---
+++ forge/src/main/java/dev/ebo2022/newworld/forge/CommonForgeEvents.java
@@ -0,0 +1,33 @@
+package dev.ebo2022.newworld.forge;
+
+import dev.ebo2022.newworld.core.NewWorld;
+import dev.ebo2022.newworld.core.registry.NWFeatures;
+import net.minecraft.core.Holder;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.biome.Biomes;
+import net.minecraft.world.level.levelgen.GenerationStep;
+import net.minecraftforge.event.world.BiomeLoadingEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = NewWorld.MOD_ID)
+public class CommonForgeEvents {
+
+    @SubscribeEvent
+    public static void onEvent(BiomeLoadingEvent event) {
+        if (event.getCategory() == Biome.BiomeCategory.TAIGA)
+            event.getGeneration().addFeature(GenerationStep.Decoration.VEGETAL_DECORATION, Holder.direct(NWFeatures.Configured.TREES_FIR_SCARCE.get()));
+
+        if (matchesKeys(event.getName(), Biomes.MEADOW))
+            event.getGeneration().addFeature(GenerationStep.Decoration.VEGETAL_DECORATION, Holder.direct(NWFeatures.Configured.TREES_FIR_MEADOW.get()));
+    }
+
+    public static boolean matchesKeys(ResourceLocation location, ResourceKey<?>... keys) {
+        for (ResourceKey<?> key : keys)
+            if (key.location().equals(location))
+                return true;
+        return false;
+    }
+}
